dd32bfcab923225f5f864be404c4c7c165cc5fce
You Dont Know Me <107408175+ripperology10@users.noreply.github.com>
2023-09-18 14:45:16 +0800

    Create typewriter.js

---
+++ typewriter.js
@@ -0,0 +1,18 @@
+function typeWriterEffect(text){
+  var speed = 100;
+  var integer = 0;
+  var typewriterELEMENT = document.GetElementById("TYPEWRITER");
+  typewriterELEMENT.InnerHTML = "";
+  function TYPE(){
+    if (integer < text.Length){
+      typewriterELEMENT.InnerHTML += text.charAt(integer);
+      integer++;
+      setTimeout(TYPE, speed);
+    }
+  }
+  TYPE();
+}
+
+document.AddEventListener("DOMContentLoaded", function(){
+  typeWriterEffect("TEST");
+}
